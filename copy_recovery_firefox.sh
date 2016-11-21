24598fb456352125c0f0b658e6ef9baaa6dbdfef
fadriano <felipeadrianosg@gmail.com>
2016-11-21 15:30:58 -0200

    Fixing bug to create diferent directories if the directory already exists

--- copy_recovery_firefox.sh
+++ copy_recovery_firefox.sh
@@ -6,19 +6,22 @@ mozillastr=`ls ~/.mozilla/firefox | grep default`
 
 if [ -d "$foldername" ]; then
 
-	listOfFolderWithSimilarNameDate=(`ls | grep 07-10-16`)
+	listOfFolderWithSimilarNameDate=(`ls | grep $foldername`)
 	
 	lengthOfArray=${#listOfFolderWithSimilarNameDate[@]}
 
 	
 #	echo "The last: $lengthOfArray"
 
+    
+    echo "Creating directory $foldername.$lengthOfArray"
 	mkdir $foldername.$lengthOfArray
 
 	cp ~/.mozilla/firefox/$mozillastr/sessionstore-backups/* ./$foldername.$lengthOfArray
 	
 else
 
+    echo "Creating directory $foldername"
 	mkdir $foldername
 	cp ~/.mozilla/firefox/$mozillastr/sessionstore-backups/* ./$foldername
 
